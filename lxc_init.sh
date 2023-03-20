f3ac660cdc56a0474030d4087130870a8877a32f
Nonac <NonacYang@gmail.com>
2023-03-20 20:23:34 +0900

    Update lxc_init.sh

--- lxc_init.sh
+++ lxc_init.sh
@@ -2,7 +2,8 @@
 
 # Get current local ip
 local_ip=`ifconfig -a|grep inet|grep 192.168.11.*|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"​`
-driver="NVIDIA-Linux-x86_64-515.86.01.run"
+#driver="NVIDIA-Linux-x86_64-515.86.01.run"
+driver="NVIDIA-Linux-x86_64-525.85.05.run"
 cuda="cuda_11.8.0_520.61.05_linux.run"
 cudaShort="cuda-11.8"
 #BUG: cudnn counld not install auto. Need manully install in container.
@@ -10,7 +11,8 @@ cudnn="cudnn-linux-x86_64-8.8.1.3_cuda11-archive.tar.xz"
 cudnnShort="cudnn-linux-x86_64-8.8.1.3_cuda11-archive"
 anaconda="Anaconda3-2022.10-Linux-x86_64.sh"
 python=3.8
-pytorch="conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia"
+pytorch="conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia"
+#pytorch="conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia"
 
 
 # Get user.csv
